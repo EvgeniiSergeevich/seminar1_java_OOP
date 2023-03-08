7ff40dfc8304186cd3aaa897d2cd6ab0e4e38834
Evgeniy <zheinirk38@gmail.com>
2023-03-08 19:32:30 +0800

    Сделал 2 ДЗ

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,52 @@
 import units.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
     public static void main(String[] args) {
         ArrayList<Human> list1 = new ArrayList<>();
-        list1.add(new Sniper("Женя"));
-        list1.add(new Witch("Коля"));
-        list1.add(new Robber("Вася"));
-        list1.add(new Crossbowman("Петя"));
-        list1.add(new Crossbowman("Толя"));
-        list1.add(new Sniper("Саша"));
-        list1.add(new Monastic("Игорь"));
-        list1.add(new Peasant("Ваня"));
-        list1.add(new Spearman("Костя"));
-        list1.add(new Spearman("Вадик"));
+//        int type = new Random().nextInt(6);
+
+        for (int i = 0; i < 9; i++) {
+            switch (new Random().nextInt(6)) {
+                case 0:
+                    list1.add(new Peasant(getName()));
+                    break;
+                case 1:
+                    list1.add(new Robber(getName()));
+                    break;
+                case 2:
+                    list1.add(new Spearman(getName()));
+                    break;
+                case 3:
+                    list1.add(new Crossbowman(getName()));
+                    break;
+                case 4:
+                    list1.add(new Sniper(getName()));
+                    break;
+                case 5:
+                    list1.add(new Monastic(getName()));
+                    break;
+                case 6:
+                    list1.add(new Witch(getName()));
+                    break;
+            }
+        }
+
+
+
+
+
+
         list1.forEach(n -> System.out.println(n.getInfo()));
     }
+
+    private static String getName()
+    {
+        return String.valueOf(Names.values()[new Random().nextInt(Names.values().length)]);
+    }
+
+
+
 }
